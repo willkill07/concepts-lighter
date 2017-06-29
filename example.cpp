4727b8c88e34b32f86b0dc1beef4ee4837298f96
Will Killian <killian4@llnl.gov>
2017-06-29 13:39:03 -0700

    Added CMake build/test/install

--- example.cpp
+++ example.cpp
@@ -27,13 +27,57 @@ using RandomAccessComparable2 = DefineConcept(
     concepts::models<RAJA::concepts::Comparable<Value<Iter>>>());
 
 #if 0
-
 // For when you want verbose errors emitted
 // -- note this is NOT SFINAE friendly
 using CheckComparable = RAJA::concepts::Comparable<A>;
 using CheckComparable2 = RandomAccessComparable2<A*>;
-
 #endif
+
+template <typename ConcreteConcept>
+bool test () {
+  return ConcreteConcept::value;
+}
+
+template <template <typename...> class UninstantiatedConcept, typename ... Args>
+bool test () {
+  return concepts::requires_<UninstantiatedConcept, Args...>::value;
+}
+
+#define START_TESTS                             \
+  int main() {                                  \
+  int totaltests=0;                             \
+  int passedtests=0;                            \
+  int failedtests=0;
+
+#define END_TESTS \
+  printf("\n\n[ %d / %d ] TESTS PASSED\n", passedtests, totaltests); \
+  return passedtests != totaltests; \
+}
+
+#define TEST(EXPECTED, ...) \
+  do { \
+    bool expected = EXPECTED; \
+    bool actual = test< __VA_ARGS__ >(); \
+    char const* const options[2] = { "[ FAILED ]", "[ PASSED ]" }; \
+    printf("%s " #__VA_ARGS__ "  -> " #EXPECTED "\n", options[expected == actual]); \
+    ++totaltests; \
+    passedtests += (expected == actual); \
+    failedtests += (expected != actual); \
+  } while(0)
+
+
+START_TESTS {
+TEST(true, RAJA::concepts::RandomAccessIterator, int*);
+TEST(true, RAJA::concepts::RandomAccessIterator, std::vector<int>::iterator);
+TEST(true, RAJA::concepts::Comparable, double);
+TEST(false, RAJA::concepts::Comparable, A);
+TEST(true, RandomAccessComparable<float*>);
+TEST(false, RandomAccessComparable<A*>);
+TEST(true, RandomAccessComparable2, float*);
+TEST(false, RandomAccessComparable2, A*);
+} END_TESTS
+
+#if 0
 
 // SFINAE example
 static_assert(concepts::requires_<RAJA::concepts::RandomAccessIterator, int*>::value, "should pass");
@@ -49,7 +93,8 @@ static_assert(RandomAccessComparable<A*>::value, "should fail");
 static_assert(concepts::requires_<RandomAccessComparable2, float*>::value, "should pass");
 static_assert(concepts::requires_<RandomAccessComparable2, A*>::value, "should fail");
 
-int
-main() {
+int main() {
   return 0;
 }
+
+#endif
